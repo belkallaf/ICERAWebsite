8a1e38f43a2196dc611f6c52a7d255c3bd211b10
btstart2020@outlook.com <btstart2020@outlook.com>
2021-10-27 13:40:57 +0300

    second change to test publish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeHeaderComponent } from './components/home-header/home-header.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeHeaderComponent,
+    WhyUsComponent,
+    PricingComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
